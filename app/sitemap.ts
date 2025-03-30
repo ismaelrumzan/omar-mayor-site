43b855a10ba718a0a763a698aa928ff81f2ab714
ismael <ismael.rumzan@gmail.com>
2025-03-30 10:31:15 -0600

    add sitemap+robot

---
+++ app/sitemap.ts
@@ -0,0 +1,30 @@
+import type { MetadataRoute } from "next"
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://www.omar4mayor.ca",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: "https://www.omar4mayor.ca/support",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.omar4mayor.ca/volunteer",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.omar4mayor.ca/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ]
+}
