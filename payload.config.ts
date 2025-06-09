99b559210447e0135646cc952346ab028312c538
ismaelrumzan <ismael.rumzan@gmail.com>
2025-06-09 00:16:14 -0600

    remove sharp

--- payload.config.ts
+++ payload.config.ts
@@ -1,5 +1,4 @@
-﻿import sharp from 'sharp'
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
+﻿import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { buildConfig } from 'payload'
 import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
@@ -26,11 +25,6 @@ export default buildConfig({
     url: process.env.DATABASE_URI || '',
     connectOptions: { dbName: process.env.PAYLOAD_DB_NAME }
   }),
-  // If you want to resize images, crop, set focal point, etc.
-  // make sure to install it and pass it to the config.
-  // This is optional - if you don't need to do these things,
-  // you don't need it!
-  sharp,
   plugins: [
     vercelBlobStorage({
       enabled: true, // Optional, defaults to true
