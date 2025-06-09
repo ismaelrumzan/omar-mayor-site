eecdfa76a52632b4c8d52ea95419cca2d2aa8309
ismael <ismael.rumzan@gmail.com>
2025-06-09 09:30:09 -0600

    Update page-content.tsx

--- components/page/page-content.tsx
+++ components/page/page-content.tsx
@@ -10,14 +10,21 @@ export function PageContent(props: PageBlocksRichContent) {
     bgStyle = `bg-${props.backgroundColor}`
   }
   let textAlign = "text-left"
+  let boxAlign = "items-start"
   if (props.textAlign) {
     textAlign = `text-${props.textAlign}`
+    if (textAlign === "text-center") {
+      boxAlign = `items-center`
+    }
+    if (textAlign === "text-right") {
+      boxAlign = `items-end`
+    }
   }
   return (
     <section className={`w-full ${bgStyle} ${textAlign}`}>
       <div className="container py-4 px-4">
         <div
-          className="prose max-w-none"
+          className={`prose max-w-none flex flex-col ${boxAlign}`}
           data-tina-field={tinaField(props, "content")}
         >
           <TinaMarkdown content={props.content} components={components} />
